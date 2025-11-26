283b266554ce998e14536953af3abbb93b72a88c
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-11-26 07:21:43 +0200

    create sign in with facebook service

--- lib/features/auth/data/firebase_auth_service.dart
+++ lib/features/auth/data/firebase_auth_service.dart
@@ -1,5 +1,6 @@
 import 'package:easacc_app/core/utils/error_handeling/custom_excption.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseAuthService {
@@ -24,6 +25,34 @@ class FirebaseAuthService {
 
       final UserCredential userCredential = await _firebaseAuth
           .signInWithCredential(credential);
+
+      final user = userCredential.user;
+
+      if (user == null) {
+        throw CustomException(
+          errMessage: "Google sign-in failed. No user returned.",
+        );
+      }
+
+      return user;
+    } on FirebaseAuthException catch (e) {
+      throw CustomException(
+        errMessage: e.message ?? "FirebaseAuth Google error.",
+      );
+    } catch (e) {
+      throw CustomException(errMessage: "Google sign-in failed: $e");
+    }
+  }
+
+  Future<User> signInWithFacebook() async {
+    try {
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+      final UserCredential userCredential = await _firebaseAuth
+          .signInWithCredential(facebookAuthCredential);
 
       final user = userCredential.user;
 
